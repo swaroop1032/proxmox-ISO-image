352cc0be804ba2359b9639d1e24a9f35b6e0204a
swaroop1032 <161610017+swaroop1032@users.noreply.github.com>
2025-11-17 15:33:44 +0530

    Create prepare-template.ps1

---
+++ packer/scripts/prepare-template.ps1
@@ -0,0 +1,6 @@
+winrm quickconfig -quiet
+Set-Service WinRM -StartupType Automatic
+
+Remove-Item -Recurse -Force C:\Windows\Temp\* -ErrorAction SilentlyContinue
+
+& "$env:SystemRoot\System32\Sysprep\Sysprep.exe" /generalize /oobe /shutdown
