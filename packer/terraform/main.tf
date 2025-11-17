755f8b50b4c7acdebd33926c9dbdc17b0079d6c7
swaroop1032 <161610017+swaroop1032@users.noreply.github.com>
2025-11-17 15:34:58 +0530

    Create main.tf

---
+++ packer/terraform/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = ">= 0.40.0"
+    }
+  }
+}
+
+variable "pm_api_url"      { default = "https://<YOUR_PVE_IP>:8006/api2/json" }
+variable "pm_api_token_id" {}
+variable "pm_api_token_secret" {}
+variable "node"            { default = "proxmox" }
+variable "vm_name"         { default = "win11-ci-01" }
+variable "storage"         { default = "local-lvm" }
+variable "bridge"          { default = "vmbr0" }
+variable "cores"           { default = 2 }
+variable "memory"          { default = 4096 }
+variable "disk_size"       { default = "60G" }
+
+provider "proxmox" {
+  pm_api_url      = var.pm_api_url
+  pm_user         = var.pm_api_token_id
+  pm_token        = var.pm_api_token_secret
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "win11" {
+  name        = var.vm_name
+  target_node = var.node
+  clone       = "tpl-win11-25h2"
+
+  agent       = 1
+  onboot      = true
+  cores       = var.cores
+  memory      = var.memory
+
+  network {
+    model  = "virtio"
+    bridge = var.bridge
+  }
+
+  disk {
+    size    = var.disk_size
+    type    = "scsi"
+    storage = var.storage
+    ssd     = 1
+  }
+
+  # Proxmox cloud-init snippets (user + network)
+  cicustom = "user=local:snippets/win11-userdata.yaml,network=local:snippets/win11-network.yaml"
+  ipconfig0 = "ip=dhcp"
+
+  boot = "order=scsi0;net0"
+}
